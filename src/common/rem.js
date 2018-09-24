a2fd7153a08dc8fb6f7d7a87112d17148a514244
guxukai <1378026744@qq.com>
2018-09-24 17:06:31 +0800

    调整rem，取消最小宽度

--- src/common/rem.js
+++ src/common/rem.js
@@ -1,8 +1,9 @@
 (function(d, w) {
   const doc = d.documentElement;
   function rem() {
-    const width = Math.min(doc.getBoundingClientRect().width, 768);
-    doc.style.fontSize = width / 7.5 + 'px';
+    // const width = Math.min(doc.getBoundingClientRect().width, 768);
+      const width = doc.getBoundingClientRect().width;
+      doc.style.fontSize = width / 7.5 + 'px';
   }
   rem();
   w.addEventListener('resize', rem);
